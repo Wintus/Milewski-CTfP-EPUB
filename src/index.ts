4a336d5d097d19d93fd19cca8f803e3b2f31979c
wintus <yuya0726+GitHub@gmail.com>
2019-10-16 02:40:42 +0900

    Add scrape of preface and chapters

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { JSDOM } from "jsdom";
 import fetch from "node-fetch";
-import { getTableOfContents } from "./dom";
+import path from "path";
+import { Chapter, Content, getContent, getTableOfContents } from "./dom";
+import { saveFile } from "./file";
 
 const url =
   "https://bartoszmilewski.com/2014/10/28/category-theory-for-programmers-the-preface/";
@@ -12,8 +14,33 @@ const fetchDoc = async (url: string) => {
   return dom.window.document;
 };
 
+const fullHTML = (html: string) =>
+  new JSDOM(`<!DOCTYPE html>${html}`).serialize();
+
+const savePage = ({ chapterNo, title, content }: Chapter & Content) => {
+  const dirPath = path.join("content", chapterNo);
+  return saveFile(
+    path.join(dirPath, `${title}.html`),
+    fullHTML(content.innerHTML)
+  );
+};
+
 (async () => {
   const document = await fetchDoc(url);
+
   const tableOfContents = getTableOfContents(document);
   console.log(tableOfContents);
+
+  const preface = savePage({
+    chapterNo: "0.0",
+    title: "Preface",
+    ...getContent(document)
+  });
+  const chapters = tableOfContents.map(async ({ url, chapterNo, title }) => {
+    const document = await fetchDoc(url);
+    await savePage({ chapterNo, title, ...getContent(document) });
+  });
+
+  await preface;
+  await Promise.all(chapters);
 })();
